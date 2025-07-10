a644b739973d8b9523761c2997b86c3d87a8db9b
kholobtseva <lyubovkholobtseva@yandex.ru>
2025-07-10 20:30:20 +0300

    API клиент как паттерн Facade

--- httpx_client.py
+++ httpx_client.py
@@ -8,6 +8,8 @@ login_payload = {
 login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json =login_payload)
 login_response_data = login_response.json()
 
+print(login_response_data)
+
 client = httpx.Client(base_url="http://localhost:8000",
                       timeout=2,
                       headers = {"Authorization" : f"Bearer {login_response_data['token']['accessToken']}"})
