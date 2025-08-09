a44fd194142f501571fa4137e0d969bc168a7f5a
Lyubov Kholobtseva <lyubovkholobtseva@yandex.ru>
2025-08-09 15:19:45 +0300

    Добавлен  pydantic_json_schema_get_use.py

---
+++ pydantic_json_schema_get_user.py
@@ -0,0 +1,36 @@
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import get_private_users_client
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import GetUserResponseSchema
+from tools.assertions.schema import validate_json_schema
+from tools.fakers import get_random_email
+
+public_users_client = get_public_users_client()
+
+create_user_request = CreateUserRequestSchema(
+    email=get_random_email(),
+    password="string",
+    last_name="string",
+    first_name="string",
+    middle_name="string"
+)
+create_user_response = public_users_client.create_user_api(create_user_request)
+create_user_response_json = create_user_response.json()
+
+print(create_user_response_json)
+
+authentication_user = AuthenticationUserSchema(
+    email=create_user_request.email,
+    password=create_user_request.password
+)
+
+
+get_user_response_schema = GetUserResponseSchema.model_json_schema()
+
+get_user_response = get_private_users_client(authentication_user).get_user_api(create_user_response_json["user"]["id"])
+get_user_response_json = get_user_response.json()
+
+validate_json_schema(instance=get_user_response_json, schema=get_user_response_schema)
+
+
